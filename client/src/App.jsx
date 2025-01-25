d2e4645908c2d39b610c2d83c1dc770ee95abd17
mariotmc <mariotomicx@gmail.com>
2025-01-25 14:32:03 +0100

    Add Rails API posts controller and React client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import PostsList from "./features/posts/PostsList";
+
+function App() {
+  return (
+    <>
+      <h1>Blog</h1>
+      <PostsList />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/features/posts/PostsList.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { API_URL } from "../../constants";
+
+function PostsList() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await fetch(API_URL);
+        if (response.ok) {
+          const json = await response.json();
+          setPosts(json);
+        } else {
+          throw new Error("Failed to fetch posts");
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id} className="post-container">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PostsList;
