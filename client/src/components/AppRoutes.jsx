a1560b46f6aea1d11896886a71ad97f109ff8676
mariotmc <mariotomicx@gmail.com>
2025-01-25 18:22:27 +0100

    Implement new post creation and update routing

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -1,15 +1,14 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PostsList from "../features/posts/PostsList";
-// import NewPost from "../features/posts/NewPost";
 import PostDetails from "../features/posts/PostDetails";
+import NewPostForm from "../features/posts/NewPostForm";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<PostsList />} />
       <Route path="/posts/:id" element={<PostDetails />} />
-      <Route path="/new" element={<h1>New Post</h1>} />
+      <Route path="/new" element={<NewPostForm />} />
     </Routes>
   );
 }
